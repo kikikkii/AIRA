da9bc8b8ea0ac75003746a920bd624c2ca1e6fc9
031802615林必涵 <948677135@qq.com>
2020-11-11 18:00:15 +0800

    init Django

--- ticketModel/admin.py
+++ ticketModel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from tickets. import Tickets
 
+
+admin.site.register(Tickets)
 # Register your models here.
--- ticketModel/models.py
+++ ticketModel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Tickets(models.Model):
+    departureDate = models.DateTimeField
+    arrivalDate = models.DateTimeField
+    flightNumber = models.CharField(max_length=20)
+    dcity = models.CharField(max_length=20)
+    acity = models.CharField(max_length=20)
+    price = models.SmallIntegerField
+    rate = models.FloatField
+    url = models.CharField(max_length=150)
 # Create your models here.
