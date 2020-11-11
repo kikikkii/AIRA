cb2dc212cd9cfcae92611d00c62016258413e716
Autism-al <70809894+Autism-al@users.noreply.github.com>
2020-11-11 19:39:44 +0800

    Add files via upload
    主页

---
+++ 前端/主页/pages/index/index.js
@@ -0,0 +1,75 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    show:false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData:['福州','厦门','泉州','漳州','三明','广州'],//下拉列表的数据
+    index:0//选择的下拉列表下标
+  },
+  //事件处理函数
+  clickMe: function(){
+    this.setData({motto:"bye world"})
+  },
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  // 点击下拉显示框
+  selectTap(){
+    this.setData({
+     show: !this.data.show
+    });
+    },
+    // 点击下拉列表
+    optionTap(e){
+    let Index=e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+     index:Index,
+     show:!this.data.show
+    });
+    },
+    onLoad: function (options) {
+    }
+})
